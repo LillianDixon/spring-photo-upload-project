458897bee80cabdb13dd5387e6cc51f68b8ffd86
Lillian Dixon <dixon.lillian92@gmail.com>
2020-04-14 11:42:47 -0600

    error fix

--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,6 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
         <Photos/>
       </div>
     );
